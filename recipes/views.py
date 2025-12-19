42bd6ac08d54b03eec51ef98e91acb6d25cb3574
Renan Alves <renannalves334@gmail.com>
2025-12-19 17:33:30 -0300

    Resolvendo alguns bugs

--- recipes/views.py
+++ recipes/views.py
@@ -5,16 +5,13 @@ from django.http import HttpResponse
 # HTTP Requests --> Response
 def home(requests): 
     return HttpResponse("Hello World, Django!")
+
 def contato(requests):
     return HttpResponse("Contato")
+
 def sobre(requests):
     return HttpResponse("Sobre")
 
 
 
-urlpatterns = [
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre)
-]
 
