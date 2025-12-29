fc1b53516a7e851a4032d0fc1da981bf24807b37
Renan Alves <renannalves334@gmail.com>
2025-12-28 22:35:25 -0300

    Adicionando Block e o for para escolhas aleatorias de title, description e afins

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from utils.recipes.factory import make_recipe
 # HTTP Requests --> Response
 def home(requests): 
     return render(requests, 'recipes/pages/home.html', context={
-        'nome': 'Renan',
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+def recipe(requests, id): 
+    return render(requests, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
     })
 
 
